4f48a5ed7436e65ccf0a688e5880517f6f22b078
Rob Laine <rlainewyllie@gmail.com>
2018-12-15 13:33:35 +1100

    Add method to convert kelvvin to celsius

--- automation/sensors/sensor.py
+++ automation/sensors/sensor.py
@@ -1,19 +1,19 @@
 # sensor file for controlling the gathering of environmental data.  
-from sense_hat import SenseHat 
-from GPIO import 
+#from sense_hat import SenseHat 
 
 
 class Sensor():
     def __init__(self, factor):
-        self.sense = SenseHat()
+        #self.sense = SenseHat()
         self.factor = factor
 
     def getRawTemp(self):
         """Returns the current temperature"""
-        return self.sense.get_temperature()
-    
-    def calibrateTemp(self, temp):
-        """Calculates the calibrated real temperature""" 
-        return (getRawTemp() - temp)
+        #return self.sense.get_temperature()
+        pass
+
+    #def calibrateTemp(self, temp):
+    #    """Calculates the calibrated real temperature""" 
+    #   return (getRawTemp() - temp)
 
 
--- automation/weather/weather.py
+++ automation/weather/weather.py
@@ -1,5 +1,6 @@
 # module for querying the weather api's for forecasts and predictions
 import requests
+import json
 
 
 class Weather():
@@ -8,10 +9,27 @@ class Weather():
         self.country_code = country_code
         self.city = city
         self.api_key = api_key
+        self.weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=melbourne,au&appid=' + self.api_key
     
     def get_weather(self):
         '''Retrieve the weather from a number of api's'''
-        pass
+        #req = requests.get(self.weather_url).json()
+        req = json.loads('{"coord": {"lon": 144.96, "lat": -37.81}, "weather": [{"id": 500, "main": "Rain", "description": "light rain", "icon": "10n"}], "base": "stations", "main": {"temp": 291.82, "pressure": 1006, "humidity": 88, "temp_min": 291.15, "temp_max": 293.15}, "visibility": 10000, "wind": {"speed": 2.1, "deg": 50}, "clouds": {"all": 75}, "dt": 1544787240, "sys": {"type": 1, "id": 9554, "message": 0.0034, "country": "AU", "sunrise": 1544727125, "sunset": 1544780265}, "id": 2158177, "name": "Melbourne", "cod": 200}')
+        #if not req.ok():
+        #   # request failed, return None so we can catch this later
+        #   return None
+        print("\n%s\n"  % (req))
+        weather = {} 
+        weather['description'] = req['weather'][0]['description']
+        # weather data is stored as a value in Kelvin (K)
+        weather['values'] = req['main']
+        # return the crucial info from the weather object
+        return weather
+    
+    def kelvin_to_cent(self,tempK):
+        '''Convert the temperature in kelvin to the equivalent 
+        in celsius'''
+        return tempK + 273.15
 
     def save_latest_call(self):
         '''Save the latest weather api call into our database'''
