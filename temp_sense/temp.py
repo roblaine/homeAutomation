51d3877eca273b2010b5383b46f641dddfe1fb28
Rob Laine <rlainewyllie@gmail.com>
2019-07-24 22:42:16 +1000

    Add method to temp script and connection details

--- temp_sense/temp.py
+++ temp_sense/temp.py
@@ -1,12 +1,17 @@
 import os, time, glob
 from datetime import datetime
 
-#os.system('modprobe w1-gpio')
-#os.system('modprobe w1-therm')
+# Set up the temperature sensors
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+# Cloud SQL Connection details
+CLOUD_SQL_ADDR='35.189.31.37'
+CLOUD_SQL_USERNAME='raspberry-pi'
  
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -23,10 +28,13 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        return '{}, {}'.format(datetime.now().strftime('%s'), temp_c)
+        return datetime.now().strftime('%s'), temp_c
 
-if __name__ == "__main__":
-    while True:
-        print(read_temp())
-        time.sleep(60)
+def save_to_cloudsql():
+    data = read_temp()
+    print(format('Saving data, {}, to cloud sql.', data))
+
+
+if __name__ == '__main__':
+    print(read_temp())
 
