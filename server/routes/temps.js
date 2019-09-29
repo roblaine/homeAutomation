e7463fcd7437b2649a9685b0b7ae00deff41cb47
Rob Laine <rlainewyllie@gmail.com>
2019-09-29 13:00:29 +1000

    Update the route to use sqllite3

--- server/routes/temps.js
+++ server/routes/temps.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database(':memory:')
 
-/* GET users listing. */
+/* GET temps listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  db.serialize(function () {
+    db.run('CREATE TABLE lorem (info TEXT)')
+    var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+
+    for (var i = 0; i < 10; i++) {
+      stmt.run('Ipsum ' + i)
+    }
+
+    stmt.finalize()
+
+    db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+      console.log(row.id + ': ' + row.info)
+    })
+  })
+
+  db.close()
+
+  res.send('Did some db stuff');
 });
 
 module.exports = router;
