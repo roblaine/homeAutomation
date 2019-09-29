849ae02ebfc650dd5b67afa6243c2f02830b44b0
Rob Laine <rlainewyllie@gmail.com>
2019-09-29 13:28:22 +1000

    Add an endpoint for getTemp by id

--- server/routes/temps.js
+++ server/routes/temps.js
@@ -1,29 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database(':memory:')
+var pgp = require('pg-promise')(/* options */)
+var db = pgp('postgres://postgres@localhost:5432/postgres')
 
 /* GET temps listing. */
 router.get('/', function(req, res, next) {
 
-  db.serialize(function () {
-    db.run('CREATE TABLE lorem (info TEXT)')
-    var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-
-    for (var i = 0; i < 10; i++) {
-      stmt.run('Ipsum ' + i)
-    }
-
-    stmt.finalize()
-
-    db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-      console.log(row.id + ': ' + row.info)
-    })
+  db.one('SELECT $1 AS value', 123)
+  .then(function (data) {
+    console.log('DATA:', data.value)
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
   })
 
-  db.close()
+  res.send('{"message": "Did some db stuff"}');
+});
 
-  res.send('Did some db stuff');
+router.get('/:id', function(req, res, next) {
+  db.one('SELECT $1 AS value', req.params.id)
+  .then(function(data) {
+    // Construct the return object
+
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
+  // Respond with the object that was requested
+  res.send(`{"id":"${req.params.id}", "data": "${data.value}"}`)
 });
 
 module.exports = router;
