7cb89686a41ecdad694b6dfff4543a482c87d4ae
Rob Laine <rlainewyllie@gmail.com>
2019-09-29 15:40:58 +1000

    Create seed sql file

---
+++ server/db/schema.sql
@@ -0,0 +1,7 @@
+-- Create the TEMPERATURES table
+CREATE TABLE TEMPERATURES(
+  id SERIAL PRIMARY KEY,
+  temperature FLOAT NOT NULL,
+  location VARCHAR(40) NOT NULL,
+  timestamp DATE NOT NULL DEFAULT NOW()
+);
