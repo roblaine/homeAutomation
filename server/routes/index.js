f2360e2636093302cb9b8850a0f8a31e681ef2cd
Rob Laine <rlainewyllie@gmail.com>
2020-01-06 12:26:06 +1100

    Add a message to the invalid index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ message: 'Invalid route. For a list of routes that are available' });
 });
 
 module.exports = router;
