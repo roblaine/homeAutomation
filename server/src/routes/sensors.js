cbc9e59e4f4b0c38682f37cf98b662c0892afbe9
Rob Laine <rlainewyllie@gmail.com>
2020-04-06 17:25:48 +1000

    Change get method to use UID

--- server/src/routes/sensors.js
+++ server/src/routes/sensors.js
@@ -3,12 +3,13 @@ const { db } = require('../db/connection');
 
 db.connect();
 
-db.query('CREATE TABLE IF NOT EXISTS \
-    sensors(id int NOT NULL AUTO_INCREMENT, \
-    location VARCHAR(50) NOT NULL, \
-    primary key(id));');
+// db.query('CREATE TABLE IF NOT EXISTS \
+//     sensors(id int NOT NULL AUTO_INCREMENT, \
+//     location VARCHAR(50) NOT NULL, \
+//     primary key(id));');
 
-router.post('/', (req, res, next) => {
+// Get the sensor info by UID
+router.get('/:uid', (req, res) => {  
   db.query(`SELECT * FROM sensors WHERE uid = ${req.body.uid}`, (err, data) => {
     if (err) {
       console.log(err);
