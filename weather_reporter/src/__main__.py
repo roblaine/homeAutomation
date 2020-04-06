f6f52f5e4d43738cfdeda5dee0ad336f0bda9114
Rob Laine <rlainewyllie@gmail.com>
2020-04-06 17:46:02 +1000

    Extract the sensor id from the response

--- weather_reporter/src/__main__.py
+++ weather_reporter/src/__main__.py
@@ -35,7 +35,7 @@ def run():
         # Get the sensor's data as a json response object
         lookup_response = requests.post(lookup_url,
             data=lookup_payload).json()
-        print(lookup_response)
+        sensor_id = lookup_response[0].uid
 
         # POST data to the server /temps/new endpoint
         temperature = sensor.read_temp()
