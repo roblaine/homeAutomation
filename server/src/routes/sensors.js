06c85c7c6f3c28c1c6f028c3129394ceb93f87cb
Rob Laine <rlainewyllie@gmail.com>
2020-04-06 17:22:53 +1000

    Add request body to DB query

--- server/src/routes/sensors.js
+++ server/src/routes/sensors.js
@@ -8,8 +8,8 @@ db.query('CREATE TABLE IF NOT EXISTS \
     location VARCHAR(50) NOT NULL, \
     primary key(id));');
 
-router.get('/', (req, res, next) => {
-  db.query(`SELECT * FROM sensors WHERE uid = ${}`, (err, data) => {
+router.post('/', (req, res, next) => {
+  db.query(`SELECT * FROM sensors WHERE uid = ${req.body.uid}`, (err, data) => {
     if (err) {
       console.log(err);
       throw (err);
