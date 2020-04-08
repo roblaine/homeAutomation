6e2820933557a94c420fbe1f3dd9337e620ece3b
Rob Laine <rlainewyllie@gmail.com>
2020-04-08 12:39:51 +1000

    Change multiline string to single line

--- server/src/routes/temps.js
+++ server/src/routes/temps.js
@@ -3,12 +3,8 @@ const { db } = require('../db/connection');
 
 db.connect();
 
-db.query('CREATE TABLE IF NOT EXISTS \
-    temps(id int NOT NULL AUTO_INCREMENT, \
-    sensor_id VARCHAR(50) NOT NULL, \
-    temperature float NOT NULL, \
-    recorded_at TIMESTAMP NOT NULL, \
-    primary key(id));');
+db.query('CREATE TABLE IF NOT EXISTS temps(id int NOT NULL AUTO_INCREMENT, sensor_id VARCHAR(50) NOT NULL, temperature float NOT NULL, recorded_at TIMESTAMP NOT NULL, primary key(id));');
+
 
 // GET all temps
 router.get('/', (req, res) => {
